6fa32a4c303b6ea6f85755ee4943b1574477bdf8
Anandita <116040996+Anandita9610@users.noreply.github.com>
2023-03-23 17:18:59 -0400

    Add files via upload

---
+++ catchat_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBdsyxL9bvacvB_PMxs3lndlef3zc3nd_k",
+    authDomain: "cat-chat-1ba7e.firebaseapp.com",
+    databaseURL: "https://cat-chat-1ba7e-default-rtdb.firebaseio.com",
+    projectId: "cat-chat-1ba7e",
+    storageBucket: "cat-chat-1ba7e.appspot.com",
+    messagingSenderId: "429776957074",
+    appId: "1:429776957074:web:68d2ec8b6f0864c436c04b",
+    measurementId: "G-Y93P5QNNYL"
+  };
